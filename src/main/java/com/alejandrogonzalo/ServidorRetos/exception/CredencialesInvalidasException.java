42f61b87ae472d10659b84f579d3624c62e0da4f
soriiaa <alex.soriaalcaraz@gmail.com>
2025-11-27 12:21:33 +0100

    Commit 8 - Creacion de excepcion personalizada para error en las credenciales

---
+++ src/main/java/com/alejandrogonzalo/ServidorRetos/exception/CredencialesInvalidasException.java
@@ -0,0 +1,9 @@
+package com.alejandrogonzalo.ServidorRetos.exception;
+
+public class CredencialesInvalidasException extends RuntimeException {
+	
+	public CredencialesInvalidasException() {
+		super("Credenciales Incorrectas");
+	}
+
+}
